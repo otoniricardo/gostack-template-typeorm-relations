b342815f8a70d9e9fa65e3c69b7f8c11038ed1fe
otoniricardo <otoniricardo@gmail.com>
2020-09-01 23:16:22 -0300

    finaliza implementacao de solucoes

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,19 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const emailExistis = await this.customersRepository.findByEmail(email);
+
+    if (emailExistis) throw new AppError('E-mail already in use');
+
+    const customer = await this.customersRepository.create({ name, email });
+
+    return customer;
   }
 }
 
---
+++ src/shared/infra/typeorm/migrations/1599003783321-AddProductIdToOrderProducts.ts
@@ -0,0 +1,36 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddProductIdToOrderProducts1599003783321
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders_products',
+      new TableColumn({
+        name: 'product_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders_products',
+      new TableForeignKey({
+        name: 'ordersProductProduct',
+        columnNames: ['product_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'products',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders_products', 'ordersProductProduct');
+    await queryRunner.dropColumn('orders_products', 'product_id');
+  }
+}
